c2a8ea3e49a676df490c71c3841d371cd3e92b1e
mtaech <mtaech@outlook.com>
2022-01-03 20:22:35 +0800

    修改文字排版

--- init.el
+++ init.el
@@ -8,7 +8,9 @@
 (require 'init-package)
 
 (custom-set-faces
- '(default ((t (:family "Sarasa Mono SC" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Sarasa Mono SC" :foundry "outline"
+                        :slant normal :weight normal
+                        :height 120 :width normal)))))
 
 ;;window-numbering on
 (window-numbering-mode t)
@@ -17,16 +19,16 @@
 ;;set gobal theme
 (load-theme 'doom-one t)
 ;;set undo-tree global
-(golobal-undo-tree-mode)
+(global-undo-tree-mode)
 ;; 启动页图片更改
 (setq dashboard-startup-banner "~/.emacs.d/asset/img/yay_evil.png")
 ;;默认全屏
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized)
-
 ;;kdb setting 快捷键设置
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c m") 'magit)
 
 
 (provide 'init.el)
