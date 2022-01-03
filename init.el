f15f058ada010010d3f78043d8e12b461bd1b57d
mtaech <mtaech@outlook.com>
2022-01-03 20:13:02 +0800

    init.el 添加注释，删除无用的代码

--- init.el
+++ init.el
@@ -10,18 +10,23 @@
 (custom-set-faces
  '(default ((t (:family "Sarasa Mono SC" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 
+;;window-numbering on
 (window-numbering-mode t)
+;;evil-mode on
 (evil-mode 1)
-(recentf-mode t)
-(setq recentf-max-menu-items 10)
+;;set gobal theme
 (load-theme 'doom-one t)
+;;set undo-tree global
+(golobal-undo-tree-mode)
 ;; 启动页图片更改
 (setq dashboard-startup-banner "~/.emacs.d/asset/img/yay_evil.png")
+;;默认全屏
+(add-hook 'emacs-startup-hook 'toggle-frame-maximized)
+
 ;;kdb setting 快捷键设置
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
-;;默认全屏
-(add-hook 'emacs-startup-hook 'toggle-frame-maximized)
+
 
 (provide 'init.el)
