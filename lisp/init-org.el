42f87ee9a0a0c11571a998fdc76c5be8a4cb3117
mtaech <mtaech@outlook.com>
2022-01-13 17:47:09 +0800

    Ð޸ÄagendaÎļþλÖÃ

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,11 +1,11 @@
 ;;org mode setting org-mode 设置
-(setq org-agenda-files '("~/Agenda/"))
+(setq org-agenda-files '("~/Documents/Agenda/"))
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/Agenda/Todo.org"  "Tasks")
+      '(("t" "Todo" entry (file+headline "~/Documents/Agenda/Todo.org"  "Tasks")
          "* TODO %?\n  %a")
-        ("j" "Journal" entry (file+datetree "~/Agenda/Journal.org" "Note")
+        ("j" "Journal" entry (file+datetree "~/Documents/Agenda/Journal.org" "Note")
          "* %U%?\n  %a")
-        ("i" "Ideas" entry (file "~/Agenda/Shower_Thought.org" "Shower_Thought")
+        ("i" "Ideas" entry (file "~/Documents/Agenda/Shower_Thought.org" "Shower_Thought")
          "* %U%?\n %a")))
 
 ;;(setq org-roam-directory (file-truename "~/org-roam"))
