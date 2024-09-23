51bf2cbc88372c5f081e4f829ba2dfe605aecf76
画野 <30824636+mtaech@users.noreply.github.com>
2024-09-23 15:09:48 +0800

    Update init-org.el

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,12 +1,12 @@
 ;;org mode setting org-mode 设置
-(setq org-agenda-files '("~/Documents/Agenda/"))
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/Documents/Agenda/Todo.org"  "Tasks")
-         "* TODO %?\n  %a")
-        ("j" "Journal" entry (file+datetree "~/Documents/Agenda/Journal.org" "Note")
-         "* %U%?\n  %a")
-        ("i" "Ideas" entry (file "~/Documents/Agenda/Shower_Thought.org" "Shower_Thought")
-         "* %U%?\n %a")))
+;;(setq org-agenda-files '("~/Documents/Agenda/"))
+;;(setq org-capture-templates
+;;      '(("t" "Todo" entry (file+headline "~/Documents/Agenda/Todo.org"  "Tasks")
+;;         "* TODO %?\n  %a")
+;;        ("j" "Journal" entry (file+datetree "~/Documents/Agenda/Journal.org" "Note")
+;;         "* %U%?\n  %a")
+;;        ("i" "Ideas" entry (file "~/Documents/Agenda/Shower_Thought.org" "Shower_Thought")
+;;         "* %U%?\n %a")))
 
 ;;(setq org-roam-directory (file-truename "~/org-roam"))
 ;;(org-roam-db-autosync-mode t)
